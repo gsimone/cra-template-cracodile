838d929d3770a3abe06235457efcca0f07313495
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-07-01 12:18:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="
+      h-screen 
+      w-screen 
+
+      bg-gray-900 
+      text-white 
+      
+      flex 
+      justify-center 
+      items-center 
+
+      text-6xl
+      font-display
+      font-black
+    ">
+      Hello world.
     </div>
   );
 }
